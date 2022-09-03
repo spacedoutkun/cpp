02df968d18f45757a37a5a8a836f74a6677fa596
Pranav Ambhore <97823606+spacedoutkun@users.noreply.github.com>
2022-09-04 00:07:09 +0530

    Add files via upload
    Second Commit

---
+++ additionStringNumberWtihIntNumber.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+{
+    string s="450";
+    stringstream ss;
+    ss<<s;
+    int k;
+    ss>>k;
+    cout<<k<<endl;
+
+    cout<<k+k;
+}
